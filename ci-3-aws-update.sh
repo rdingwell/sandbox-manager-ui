73ae13fe236f188502ec92e1e8265129cfb9e5f1
jacobC12 <jacob@interopion.com>
2018-02-12 13:48:18 -0700

    Hard coding the TARGET_AWS_CLUSTER to hspc-test

--- ci-3-aws-update.sh
+++ ci-3-aws-update.sh
@@ -9,11 +9,11 @@ echo "TASK_VERSION: $TASK_VERSION"
 echo "BITBUCKET_BRANCH: $BITBUCKET_BRANCH"
 if [ ! -z "$BITBUCKET_BRANCH" ] && [ "$BITBUCKET_BRANCH" == "develop" ]
 then
-    [[ -z "$TARGET_AWS_CLUSTER" ]] && { echo "Error: TARGET_AWS_CLUSTER is not provided"; exit 1; } || echo "TARGET_AWS_CLUSTER: $TARGET_AWS_CLUSTER"
+#    [[ -z "$TARGET_AWS_CLUSTER" ]] && { echo "Error: TARGET_AWS_CLUSTER is not provided"; exit 1; } || echo "TARGET_AWS_CLUSTER: $TARGET_AWS_CLUSTER"
     [[ -z "$TARGET_AWS_SERVICE" ]] && { echo "Error: TARGET_AWS_SERVICE is not provided"; exit 1; } || echo "TARGET_AWS_SERVICE: $TARGET_AWS_SERVICE"
 
     echo "updating aws esc service..."
-    aws ecs update-service --cluster $TARGET_AWS_CLUSTER --service $TARGET_AWS_SERVICE --task-definition $PROJECT_NAME:$TASK_VERSION
+    aws ecs update-service --cluster hspc-test --service $TARGET_AWS_SERVICE --task-definition $PROJECT_NAME:$TASK_VERSION
 else
     echo "skipping deployment"
 fi
