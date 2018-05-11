51d2f1dfe8c504014495fbe4ea15af6554f3d215
Travis Cummings <travis@interopion.com>
2018-05-11 17:28:07 -0600

    Using hspc-node-base-image

--- ci/ci-5-aws-service-update.sh
+++ ci/ci-5-aws-service-update.sh
@@ -6,5 +6,4 @@ export AWS_TASK_DEFINITION=$(aws ecs describe-task-definition --region us-west-2
 export AWS_TASK_REVISION=$(echo ${AWS_TASK_DEFINITION} | jq --raw-output '.taskDefinition.revision')
 
 # update the service to this task definition
-#echo $(aws ecs update-service --region us-west-2 --cluster hspc-${TARGET_ENV} --service ${PROJECT_FULL_NAME} --task-definition ${PROJECT_FULL_NAME}:${AWS_TASK_REVISION})
-echo $(aws ecs update-service --region us-west-2 --cluster hspc-${TARGET_ENV} --service sandbox-manager-test --task-definition ${PROJECT_FULL_NAME}:${AWS_TASK_REVISION})
+echo $(aws ecs update-service --region us-west-2 --cluster hspc-${TARGET_ENV} --service ${PROJECT_FULL_NAME} --task-definition ${PROJECT_FULL_NAME}:${AWS_TASK_REVISION})
