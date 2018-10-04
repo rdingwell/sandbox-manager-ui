6a2f0567d446221e9bf85dda9f4e6a6757f9258d
ddimitrov-interopion <dimitar@interopion.com>
2018-10-04 15:07:27 +0300

    SNDBX-835

--- projects/sandbox-manager/src/redux/reducers/sandbox/index.js
+++ projects/sandbox-manager/src/redux/reducers/sandbox/index.js
@@ -214,7 +214,6 @@ export default function (state = initialState, action) {
             state.importResults = undefined;
             state.launchScenarios = undefined;
             state.notifications = [];
-            state.sandboxes = [];
             break;
     }
 
