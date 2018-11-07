1d5b68a62e5a3d4d3ef9d19855f19b4500056ea8
d.dimitrov <dimitar@interopion.com>
2018-11-08 00:29:26 +0200

    test

--- projects/sandbox-manager/src/components/containers/Apps/index.js
+++ projects/sandbox-manager/src/components/containers/Apps/index.js
@@ -191,21 +191,19 @@ class Apps extends Component {
         event && event.preventDefault();
         event && event.stopPropagation();
 
-        if(!!app) {
-            this.props.loadApp(app, loadedApp => {
-                let clientJSON = JSON.parse(loadedApp.clientJSON);
-                let isPatientScoped = clientJSON.scope.find(i => i.toLowerCase().indexOf('patient/') >= 0);
-                if (isPatientScoped) {
-                    app && app.samplePatients && this.props.fetchPersonas(PersonaList.TYPES.patient, app.samplePatients.split('?')[1], 15);
-                    (!app || !app.samplePatients) && this.props.fetchPersonas(PersonaList.TYPES.patient, null, 15);
-                    this.setState({ appToLaunch: app, registerDialogVisible: false});
-                    this.props.resetPersonas();
-                } else {
-                    this.setState({ appToLaunch: app, registerDialogVisible: false}, this.doLaunch);
-                }
-            });
+        if (!!app) {
+            let clientJSON = JSON.parse(app.clientJSON);
+            let isPatientScoped = clientJSON.scope.find(i => i.toLowerCase().indexOf('patient/') >= 0);
+            if (isPatientScoped) {
+                app && app.samplePatients && this.props.fetchPersonas(PersonaList.TYPES.patient, app.samplePatients.split('?')[1], 15);
+                (!app || !app.samplePatients) && this.props.fetchPersonas(PersonaList.TYPES.patient, null, 15);
+                this.setState({ appToLaunch: app, registerDialogVisible: false });
+                this.props.resetPersonas();
+            } else {
+                this.setState({ appToLaunch: app, registerDialogVisible: false }, this.doLaunch);
+            }
         } else {
-            this.setState({ appToLaunch: undefined, registerDialogVisible: false});
+            this.setState({ appToLaunch: undefined, registerDialogVisible: false });
         }
     };
 }
