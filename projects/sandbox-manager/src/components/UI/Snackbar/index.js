36ca6a45422353be9243136c38fb74625d94e262
Jacob Crump <>
2018-12-13 13:56:25 -0700

    OperationOutcome errors will now be parsed to only show relevant information on popup snackbars.

--- projects/sandbox-manager/src/components/UI/Snackbar/index.js
+++ projects/sandbox-manager/src/components/UI/Snackbar/index.js
@@ -8,12 +8,27 @@ class Snackbar extends Component {
     render () {
         return <div className='error-message-wrapper'>
             <span className='custom-snack' style={{ margin: '0 auto', backgroundColor: this.props.theme.palette.primary4Color, color: this.props.theme.palette.primary5Color }}>
-                {this.props.message.toString()}
+                {this.parseMessage(this.props.message).toString()}
                 <IconButton className='error-close-button' onClick={this.props.onClose}>
                     <CloseIcon color={this.props.theme.palette.primary5Color}/>
                 </IconButton>
             </span>
         </div>;
+    }
+
+    parseMessage = (message) => {
+        try {
+            message = JSON.parse(message);
+            if (message.resourceType !== undefined && message.resourceType !== null) {
+                if (message.resourceType === "OperationOutcome") {
+                    message = message.issue[0].diagnostics;
+                }
+            }
+        } catch(e) {
+            // do nothing
+        }
+
+        return message;
     }
 }
 
