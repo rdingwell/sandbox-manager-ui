70c16625866b77497371693c1055ce8bf8fed535
d.dimitrov <dimitar@interopion.com>
2019-01-31 16:54:09 +0200

    Updates to the cookie deletion.

--- projects/sandbox-manager/src/components/containers/Dashboard/index.js
+++ projects/sandbox-manager/src/components/containers/Dashboard/index.js
@@ -99,7 +99,7 @@ class Dashboard extends Component {
     };
 
     handleCancel = () => {
-        document.cookie = "hspc-invitation-id=Thu, 01 Jan 1970 00:00:00 GMT; domain=.hspconsortium.org; path=/";
+        document.cookie = "hspc-invitation-id=;expires=Thu, 01 Jan 1970 00:00:00 GMT; domain=.hspconsortium.org; path=/";
         this.setState({ showAccept: false, invitationToAccept: undefined });
     };
 }
