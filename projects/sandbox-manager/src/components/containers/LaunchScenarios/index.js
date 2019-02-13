f8be7934d5f640964fa0edbe318794d97b0b3931
d.dimitrov <dimitar@interopion.com>
2019-02-13 15:58:44 +0200

    tmp changes

--- projects/sandbox-manager/src/components/containers/LaunchScenarios/index.js
+++ projects/sandbox-manager/src/components/containers/LaunchScenarios/index.js
@@ -31,6 +31,7 @@ import {
     getDefaultUserForSandbox
 } from '../../../redux/action-creators';
 import { connect } from 'react-redux';
+import Cookies from 'js-cookie';
 import cookie from 'react-cookies';
 import { bindActionCreators } from 'redux';
 import withErrorHandler from 'sandbox-manager-lib/hoc/withErrorHandler';
@@ -189,7 +190,9 @@ class LaunchScenarios extends Component {
         date.setTime(date.getTime() + (3 * 60 * 1000));
 
         let testCookie = cookie;
+        let testCookie2 = Cookies;
         testCookie.save('hspc-launch-token', JSON.stringify(token), { path: '/', domain: cookieUrl, expires: date.getTime() });
+        testCookie2.set('hspc-launch-token', JSON.stringify(token), { path: '/', domain: cookieUrl, expires: date.getTime() });
 
         let openLink = this.refs.openLink;
         openLink.href = `${this.props.ehrUrl}/launch`;
