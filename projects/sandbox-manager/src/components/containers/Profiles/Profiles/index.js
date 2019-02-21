85c59b458d6ea88d507bcd9949f843ee46c8f8a0
d.dimitrov <dimitar@interopion.com>
2019-02-21 17:25:43 +0200

    Changes to the profiles screen

--- projects/sandbox-manager/src/components/containers/Profiles/Profiles/index.js
+++ projects/sandbox-manager/src/components/containers/Profiles/Profiles/index.js
@@ -6,44 +6,63 @@ import './styles.less';
 class Profiles extends Component {
     render () {
         return <div className='profiles-tab-wrapper'>
-            <Card className='card profile-list-wrapper'>
+            <Card className='card info'>
                 <CardTitle className='card-title'>
-                    <span>Available Profiles</span>
+                    <span>Info</span>
                 </CardTitle>
                 <div className='card-content url-list-wrapper'>
-                    {this.props.profiles && this.getList()}
+                    <p>
+                        A profile is a set of constraints on a resource represented as a StructureDefinition which often has references to ValueSet, CodeSystem and SearchParameter resources.
+                        Thus, in order to successfully validate against a given profile (aka a StructureDefinition resource), your sandbox will need the StructureDefinition along with each referenced ValueSet,
+                        CodeSystem and SearchParameter uploaded to the sandbox’s FHIR server. You can do so by uploading a zip file containing all of the needed resources using the button below.
+                        We are currently only accepting zip files that contain resources that are in JSON format.
+                    </p>
+                    <p>
+                        There are several publicly available profiles that you can download. The QI-Core and US-Core are among the most popular profiles. Below are the instructions on how to upload them to your
+                        sandbox.
+                    </p>
+                    <p>
+                        How to upload the QI-Core profile:
+                        <ol>
+                            <li>Find the QI-Core Downloads page and download the “JSON” Definitions zip file.</li>
+                            <li>Upload this zip file from your machine using the Upload Profile button.</li>
+                        </ol>
+                    </p>
+                    <p>
+                        How to upload the US-Core profile:
+                        <ol>
+                            <li>Download the package.tgz from their downloads page.</li>
+                            <li>Unzip the above file and navigate to the “Package” folder.</li>
+                            <li>Zip this “Package” folder and upload to your sandbox using the Upload Profile button.</li>
+                        </ol>
+                    </p>
+                    <p>
+                        You can also upload your own custom profile by aggregating all of the required StructureDefinition, ValueSet, CodeSystem and SearchParameters resources that you have created in JSON
+                        format in a zip file.
+                    </p>
                 </div>
             </Card>
-            <Card className='card profiles-actions-wrapper'>
+            <Card className='card profile-list-wrapper'>
                 <CardTitle className='card-title'>
-                    <span>Import profile</span>
+                    <span>Profiles</span>
                 </CardTitle>
                 <div className='card-content import-button'>
-                    <input type='file' id='fileZip' ref='fileZip' style={{ display: 'none' }} onChange={this.loadZip}/>
-                    <RaisedButton label='Load Profile (zip file)' primary onClick={() => this.refs.fileZip.click()}/>
+                    <div className='file-load-wrapper'>
+                        <input type='file' id='fileZip' ref='fileZip' style={{ display: 'none' }} onChange={this.loadZip}/>
+                        <RaisedButton label='Load Profile (zip file)' primary onClick={() => this.refs.fileZip.click()}/>
+                    </div>
+                    <div className='loaded-profiles-wrapper'>
+                        {this.props.profiles && this.getList()}
+                    </div>
                 </div>
             </Card>
         </div>
     }
 
     getList = () => {
-        let profiles = Object.keys(this.props.profiles);
         return <List>
-            {profiles.map((profile, key) => {
-                //Group the items
-                let grouping = {};
-                this.props.profiles[profile].map(item => {
-                    !grouping[item.resourceType] && (grouping[item.resourceType] = []);
-                    grouping[item.resourceType].push(item);
-                });
-
-                //Build child components
-                let nestedItems = Object.keys(grouping).map((i, k) => {
-                    return <ListItem key={k} className='profile-list-item' primaryText={i} initiallyOpen={false} primaryTogglesNestedList hoverColor='transparent'
-                                     nestedItems={grouping[i].map(q => <ListItem className='profile-list-item' key={q.id} primaryText={q.title} hoverColor='transparent'/>)}/>
-                });
-
-                return <ListItem key={key} className='profile-list-item' primaryText={profile} initiallyOpen={false} primaryTogglesNestedList nestedItems={nestedItems} hoverColor='transparent'/>;
+            {this.props.profiles.map((profile, key) => {
+                return <ListItem key={key} className='profile-list-item' primaryText={profile.url} hoverColor='transparent'/>;
             })}
         </List>
     };
