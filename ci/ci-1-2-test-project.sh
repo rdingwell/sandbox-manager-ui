cf38906fa0ac9e6b3e756282ec19f248d3136a37
d.dimitrov <dimitar@interopion.com>
2019-05-13 10:19:02 +0300

    Add it to apt archives to pull dependencies.

--- ci/ci-1-2-test-project.sh
+++ ci/ci-1-2-test-project.sh
@@ -9,10 +9,11 @@ echo "Installing needed libs"
 apt-get install -y wget
 
 echo "Fetching chrome"
+cd /var/cache/apt/archives/
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 echo "Installing chrome"
-dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install
+apt-get install google-chrome-stable
 
 echo "Chrome check:"
 google-chrome --version
