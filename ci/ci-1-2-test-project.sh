b21a63cf81a5e6d75c40578c33701fd517f26ac9
d.dimitrov <dimitar@interopion.com>
2019-05-13 10:05:02 +0300

    Added dependencies in the install list

--- ci/ci-1-2-test-project.sh
+++ ci/ci-1-2-test-project.sh
@@ -4,8 +4,8 @@ echo "Setting up the ENV"
 printf "deb http://archive.debian.org/debian/ jessie main\ndeb-src http://archive.debian.org/debian/ jessie main\ndeb http://security.debian.org jessie/updates main\ndeb-src http://security.debian.org jessie/updates main" > /etc/apt/sources.list
 apt-get update
 
-echo "Installing wget"
-apt-get install -y wget #xvfb
+echo "Installing needed libs"
+apt-get install -y wget fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libatspi2.0-0 libcups2 libdbus-1-3 libgtk-3-0 #xvfb
 
 echo "Fetching chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
