b3a3e2ada858deeecdc2d85f7e32314b16047474
d.dimitrov <dimitar@interopion.com>
2019-05-13 09:54:47 +0300

    Added update before the install.

--- ci/ci-1-2-test-project.sh
+++ ci/ci-1-2-test-project.sh
@@ -10,12 +10,8 @@ apt-get install -y wget xvfb
 echo "Creating FF folder"
 mkdir $FIREFOX_DIR
 
-echo "Fetching ff"
-wget -q --continue --output-document $FIREFOX_FILENAME "https://ftp.mozilla.org/pub/firefox/releases/${FIREFOX_VERSION}/linux-x86_64/en-US/firefox-${FIREFOX_VERSION}.tar.bz2"
-tar -xaf "$FIREFOX_FILENAME" --strip-components=1 --directory "$FIREFOX_DIR"
-
-echo "Add ff to the path"
-export PATH=$FIREFOX_DIR:$PATH
+echo "Installing FF"
+apt-get install firefox
 
 echo "FF check:"
 firefox -v
