0f055a9a4385663b1cf847bbdc9bcffa933354e2
d.dimitrov <dimitar@interopion.com>
2019-05-13 09:50:19 +0300

    Added update before the install.

--- ci/ci-1-2-test-project.sh
+++ ci/ci-1-2-test-project.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Installing xvfb..."
+echo "Setting up the ENV"
+printf "deb http://archive.debian.org/debian/ jessie main\ndeb-src http://archive.debian.org/debian/ jessie main\ndeb http://security.debian.org jessie/updates main\ndeb-src http://security.debian.org jessie/updates main" > /etc/apt/sources.list
 apt-get update
+
+echo "Installing xvfb..."
 apt-get install -y wget xvfb
 
 echo "Creating FF folder"
