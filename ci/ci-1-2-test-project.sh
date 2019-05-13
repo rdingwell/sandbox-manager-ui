4cf7aa9a6b32350a42f5ac9b707f5ff98d4cda05
d.dimitrov <dimitar@interopion.com>
2019-05-13 10:02:16 +0300

    Search for chrome.

--- ci/ci-1-2-test-project.sh
+++ ci/ci-1-2-test-project.sh
@@ -3,15 +3,15 @@
 echo "Setting up the ENV"
 printf "deb http://archive.debian.org/debian/ jessie main\ndeb-src http://archive.debian.org/debian/ jessie main\ndeb http://security.debian.org jessie/updates main\ndeb-src http://security.debian.org jessie/updates main" > /etc/apt/sources.list
 apt-get update
-apt-cache search chrome
-echo "Installing xvfb..."
-#apt-get install -y xvfb
 
-echo "Creating FF folder"
-#mkdir $FIREFOX_DIR
+echo "Installing wget"
+apt-get install -y wget #xvfb
 
-echo "Installing FF"
-#apt-get install firefox
+echo "Fetching chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
-echo "FF check:"
-#firefox -v
+echo "Installing chrome"
+dpkg -i google-chrome-stable_current_amd64.deb
+
+echo "Chrome check:"
+google-chrome --version
