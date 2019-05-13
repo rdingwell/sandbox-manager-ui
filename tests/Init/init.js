76887394db3d2caf6d2ca1d7b230d5309c855086
d.dimitrov <dimitar@interopion.com>
2019-05-13 13:38:12 +0300

    Figuring out the display problem

--- tests/Init/init.js
+++ tests/Init/init.js
@@ -18,7 +18,7 @@ exports.init = describe('Initialize the test suite', function () {
     it('should wait for the driver to start', () => d.then(_d => driver = _d));
 
     it('should initialize the context', async () => {
-        await driver.manage().window().maximize();
+        // await driver.manage().window().maximize();
         await driver.get(rootURL);
         setDriver(driver);
     });
