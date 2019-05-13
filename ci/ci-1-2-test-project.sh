d02fddd7cd89215fdffe11758ca4ef8e2e7dc544
d.dimitrov <dimitar@interopion.com>
2019-05-13 09:57:02 +0300

    Search for firefox.

--- ci/ci-1-2-test-project.sh
+++ ci/ci-1-2-test-project.sh
@@ -3,15 +3,15 @@
 echo "Setting up the ENV"
 printf "deb http://archive.debian.org/debian/ jessie main\ndeb-src http://archive.debian.org/debian/ jessie main\ndeb http://security.debian.org jessie/updates main\ndeb-src http://security.debian.org jessie/updates main" > /etc/apt/sources.list
 apt-get update
-
+apt-cache search firefox
 echo "Installing xvfb..."
-apt-get install -y wget xvfb
+#apt-get install -y xvfb
 
 echo "Creating FF folder"
-mkdir $FIREFOX_DIR
+#mkdir $FIREFOX_DIR
 
 echo "Installing FF"
-apt-get install firefox
+#apt-get install firefox
 
 echo "FF check:"
-firefox -v
+#firefox -v
