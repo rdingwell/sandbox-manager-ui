ccbb5465c7f26c7f2a295fd802d6b0e86783c7d5
dimitar <dimitar@interopion.com>
2020-08-17 15:21:38 +0300

    SNDBX-1465

--- ci/ci-1-prepare-sources.sh
+++ ci/ci-1-prepare-sources.sh
@@ -14,16 +14,16 @@ cat ${TEMPLATE_FILE}
 
 echo "starting prepare_build.sh..."
 
-echo "Cloning the 'lib' repo"
-cd ../projects
-git clone git@bitbucket.org:hspconsortium/sandbox-manager-lib.git
-cd sandbox-manager-lib
-npm link
-cd ../../
+#echo "Cloning the 'lib' repo"
+#cd ../projects
+#git clone git@bitbucket.org:hspconsortium/sandbox-manager-lib.git
+#cd sandbox-manager-lib
+#npm link
+#cd ../../
 
-echo "::: Running NPM INSTALL"
-npm install
-npm link sandbox-manager-lib
+#echo "::: Running NPM INSTALL"
+#npm install
+#npm link sandbox-manager-lib
 
 cd ci
 
